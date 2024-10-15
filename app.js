83428d6a79a960da392fc0849707b61b7d64881e
Alejandro Gispert <158798324+AlejandroGispert@users.noreply.github.com>
2024-10-15 08:54:42 +0200

    recovery commit2

--- app.js
+++ app.js
@@ -35,11 +35,24 @@ app.use(
   })
 );
 
+// Event listeners for mongoose connection
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose connected to the database");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("Mongoose connection error: " + err);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose disconnected from the database");
+});
+
 // here is the accesslink to the mongodb database, to modify on nodemon.json
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.5aeob.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.5aeob.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority&appName=Cluster0`
   )
   .then(() => {
     app.listen(8000, function () {
